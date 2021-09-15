4d47aa168583d3f0c23708819e27f7d6e4ccdb6d
EmilioOcelotl <pi@raspberrypi>
2021-09-15 05:38:56 +0100

    inicio toxica

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -2,60 +2,79 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-	ofSetVerticalSync(true);
-	ofSetLogLevel(OF_LOG_VERBOSE);
+  
+  ofSetVerticalSync(true);
+  ofSetLogLevel(OF_LOG_VERBOSE);
 
+  //printer.open("/dev/tty.PL2303-00002014");
+  printer.open("/dev/serial0");    
+  // img.loadImage("tox3.jpeg");
+  //   video.initGrabber(640, 480);
 
-    //printer.open("/dev/tty.PL2303-00002014");
-    printer.open("/dev/serial0");    
-
-    img.loadImage("tox3.jpeg");
-    video.initGrabber(640, 480);
+  modo = 0; 
+  
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-	video.update();
+  //	video.update();
+  
+  if(contador % 200 == 0){
+      choose(); 
+    }
+
+    contador++;
+    //ofLog(OF_LOG_NOTICE, ofToString(contador)); 
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+  
     ofBackground(0);
-    video.draw(0,0);
+    //video.draw(0,0);
+
+
+    
 }
 
 void ofApp::exit(){
     printer.close();
 }
 
+void ofApp::choose(){
+  ofLog(OF_LOG_NOTICE, ofToString((int)ofRandom(108))+".jpg");
+}
+
 //--------------------------------------------------------------
 void ofApp::keyPressed  (int key){ 
-	if(key == ' '){
-        printer.print(video);
-    } else if (key == 't'){
-        printer.println("Hello World!!");
-    } else if (key == 'r'){
-        printer.setReverse(true);
-        printer.println("Reverse ON");
-        printer.setUnderline(true);
-    } else if(key == 'b'){
-        printer.setBold(true);
-        printer.println("Bold ON");
-        printer.setBold(false);
-    } else if(key == 'u'){
-        printer.setUpDown(true);
-        printer.println("UpDown ON");
-        printer.setUpDown(false);
-    } else if(key == 'l'){
-        printer.setUnderline(true);
-        printer.println("Underline ON");
-        printer.setUnderline(false);
-    } else if(key == 'd'){
-        printer.setDoubleWidth(true);
-        printer.println("Double Width ON");
-        printer.setDoubleWidth(false);
-    } else if(key == 'a'){
-        printer.setAlign(LEFT);
+  /*
+  if(key == ' '){
+    // printer.print(video);
+  } else if (key == 't'){
+    printer.println("Hello World!!");
+  } else if (key == 'r'){
+    printer.setReverse(true);
+    printer.println("Reverse ON");
+    printer.setUnderline(true);
+  } else if(key == 'b'){
+    printer.setBold(true);
+    printer.println("Bold ON");
+    printer.setBold(false);
+  } else if(key == 'u'){
+    printer.setUpDown(true);
+    printer.println("UpDown ON");
+    printer.setUpDown(false);
+  } else if(key == 'l'){
+    printer.setUnderline(true);
+    printer.println("Underline ON");
+    printer.setUnderline(false);
+  } else if(key == 'd'){
+    printer.setDoubleWidth(true);
+    printer.println("Double Width ON");
+    printer.setDoubleWidth(false);
+  } else if(key == 'a'){
+    printer.setAlign(LEFT);
         printer.println("left align");
         printer.setAlign(MIDDLE);
         printer.println("middle align");
@@ -73,6 +92,7 @@ void ofApp::keyPressed  (int key){
         printer.println("by Patricio Gonzalez Vivo.com");
         //printer.print(img,127);
     }
+  */ 
 }
 
 //--------------------------------------------------------------
--- src/ofApp.h
+++ src/ofApp.h
@@ -2,27 +2,34 @@
 
 #include "ofMain.h"
 #include "ofxThermalPrinter.h"
+#include "ofxGPIO.h"
+//#include "ofxRPiCameraVideoGrabber.h"
 
 class ofApp : public ofBaseApp{
 public:
-    void setup();
-    void update();
-    void draw();
-    void exit();
-    
-    void keyPressed  (int key);
-    void keyReleased(int key);
-    void mouseMoved(int x, int y );
-    void mouseDragged(int x, int y, int button);
-    void mousePressed(int x, int y, int button);
-    void mouseReleased(int x, int y, int button);
-    void windowResized(int w, int h);
-    void dragEvent(ofDragInfo dragInfo);
-    void gotMessage(ofMessage msg);
+  void setup();
+  void update();
+  void draw();
+  void exit();
+  void choose(); 
+  
+  void keyPressed  (int key);
+  void keyReleased(int key);
+  void mouseMoved(int x, int y );
+  void mouseDragged(int x, int y, int button);
+  void mousePressed(int x, int y, int button);
+  void mouseReleased(int x, int y, int button);
+  void windowResized(int w, int h);
+  void dragEvent(ofDragInfo dragInfo);
+  void gotMessage(ofMessage msg);
 	
-	ofxThermalPrinter printer;
-    ofImage img;
-    
-    ofVideoGrabber  video;
+  ofxThermalPrinter printer;
+  ofImage img;
+  int modo;
+  int contador;
+  string archivo; 
+  // ofVideoGrabber  video;
+  // ofxOMXVideoGrabber videoGrabber;
+
 };
 
